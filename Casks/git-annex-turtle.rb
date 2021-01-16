6349421b51cfcd86e666f829e5f83b9841bc5b4d
William Goeller <william@williamgoeller.com>
2021-01-16 14:47:41 -0800

    fix sha256 hash

--- Casks/git-annex-turtle.rb
+++ Casks/git-annex-turtle.rb
@@ -1,8 +1,8 @@
 cask "git-annex-turtle" do
   version "0.2"
-  sha256 "3e383f673183265700e0c13a5f7b1d3348247465c3d8817c845d62e4c92d86ce"
+  sha256 "7fd0db32ec58523f2cd5bc6e3df3654824070d59a0d72cb11aadab4561ec6a46"
 
-  url "https://github.com/andrewringler/git-annex-turtle/releases/download/v#{version}/git-annex-turtle-0.2.dmg"
+  url "https://github.com/andrewringler/git-annex-turtle/releases/download/v#{version}/git-annex-turtle-#{version}.dmg"
   appcast "https://github.com/andrewringler/git-annex-turtle/releases.atom"
   name "git-annex-turtle"
   desc "Provides Apple Finder integration for git-annex on macOS"
